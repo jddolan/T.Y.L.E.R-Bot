01a6583ebf4faddbe4fe2716eec839f2b26f088a
jddolan <jdolan@disruptops.com>
2021-01-16 12:28:23 -0600

    support multiple channels

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 class Tyler:
-    id: int = 130402289755095041
     msgCount: int = 0
     msgsSinceLastResponse: int = 0
     channel = None
@@ -50,96 +49,39 @@ class Tyler:
         self.lastResponse = None
         self.msgsSinceLastResponse = 0
 
-tyler = Tyler()
+tyler: dict = {}
 
 @client.event
 async def on_message(message):
     if message.content[0] == '!':
-        command = message.content.split(' ')[0]
-        if command == "!scan":
-            await scan(message)
-        elif command in commands.keys():
-            if command == '!quote':
-                await quote(message)
-            elif command == '!addquote':
-                await addQuote(message)
-            else:
-                await message.channel.send(commands[command])
-        else:
-            await message.channel.send("Command not found, type !help for a list of all valid commands")
-    elif tyler.channel == None or tyler.channel == message.channel:
-        if message.author.id == tyler.id:
-            print("it's a message from tyler")
-            if tyler.msgCount == 0:
-                print("it's the first one")
-                tyler.channel = message.channel
-                tyler.startTime = message.created_at
-
-            tyler.messages.append(message.content)
-
-            tyler.msgCount += 1
-            tyler.msgsSinceLastResponse += 1
-
-            print(f"tyler has now sent {tyler.msgCount} messages in a row")
-
-            def check(msg):
-                print(f"went in the check, variables: \nmsgCount: {tyler.msgCount}\nmsgLimit: {msgLimit}\nauthor id: {msg.author.id}\ntyler id: {tyler.id}\n")
-                return msg.author.id != tyler.id or (tyler.msgCount >= msgLimit + 1)
-
-            if tyler.msgCount >= msgLimit and tyler.msgsSinceLastResponse >= responseRateCap:
-                print("msgCount is higher than the limit")
-                tyler.diff = int(message.created_at.timestamp()) - int(tyler.startTime.timestamp())
-                if tyler.diff >= timeoutLength and tyler.waiting == False:
-                    print("the time difference was higher than the time limit and we are not waiting on another message to be sent")
-                    if tyler.msgsSinceLastResponse >= responseRateCap:
-                        tyler.msgsSinceLastResponse = 0
-                        await tyler.channel.send(response(tyler.messages))
-                else:
-                    print("the messages were sent within the time limit")
-                    if tyler.waiting == False:
-                        print("we are not waiting on another message")
-                        try:
-                            print("now waiting on the check...")
-                            tyler.waiting = True
-                            msg = await client.wait_for('message', timeout=timeoutLength - tyler.diff, check=check)
-                            if (msg.author.id != tyler.id):
-                                print("someone else sent a message, abort mission")
-                                tyler.reset()
-                            else:
-                                print("sending message")
-                                tyler.msgsSinceLastResponse = 0
-                                await tyler.channel.send(response(tyler.messages))
-                        except asyncio.TimeoutError:
-                            print("timed out, sending message")
-                            tyler.msgsSinceLastResponse = 0
-                            await tyler.channel.send(response(tyler.messages))
-                        print("resetting the waiting variable")
-                        tyler.waiting = False
-
+        command(message)
+    elif tyler.get(message.channel.name, None) == None:
+        # New channel found, create new dict entry
+        tyler[message.channel.name] = Tyler(channel=message.channel)
+    else:
+        if message.author.id == tylerId:
+            tylerMessage(message)
 
         elif message.author != client.user:
             print("message not from tyler found, abort mission")
-            tyler.reset()
+            tyler[message.channel.name].reset()
             if message.content == 'pee' or message.content == 'poo':
                 await message.channel.send("Stop saying pee and poo it's not as funny as you think it is.")
 
-
-    
-
 def response(messages):
 
-    if tyler.msgCount <= 5: 
+    if tyler[message.channel.name].msgCount <= 5: 
         severity = "mild"
-    elif tyler.msgCount > 5 and tyler.msgCount <= 10:
+    elif tyler[message.channel.name].msgCount > 5 and tyler[message.channel.name].msgCount <= 10:
         severity = "moderate"
     else:
         severity = "severe"
 
     response = random.choice(responses[severity])
-    while response == tyler.lastResponse:
+    while response == tyler[message.channel.name].lastResponse:
         response = random.choice(responses[severity])
 
-    tyler.lastResponse = response
+    tyler[message.channel.name].lastResponse = response
     if response == "spongebob":
         response = spongebob(messages)
     return response
@@ -156,6 +98,43 @@ def spongebob(messages):
         newmessage = newmessage + "".join(msg) + "\n"
         
     return(newmessage)
+
+async def command(message):
+    command = message.content.split(' ')[0]
+    if command == "!scan":
+        await scan(message)
+    elif command in commands.keys():
+        if command == '!quote':
+            await quote(message)
+        elif command == '!addquote':
+            await addQuote(message)
+        else:
+            await message.channel.send(commands[command])
+    else:
+        await message.channel.send("Command not found, type !help for a list of all valid commands")
+    return
+
+async def scan(message):
+    count = 0
+    print(f"id: {message.id}")
+    iterator = message
+    oldIterator = "temp"
+    while(True):
+        async for msg in message.channel.history(limit=9999999999,before=iterator):
+            count += 1
+            iterator = msg
+            if '"-' in msg.content or '" -' in msg.content:
+                #  or "-" in msg.content:
+                print(msg.content)
+                print(f"count: {count}\n")
+        if oldIterator == iterator:
+            print("done scanning")
+            break
+        oldIterator = iterator
+        print(f"last message: {msg}")
+        print(msg.content)
+        print("fetching more results")
+        print(f"iterator: {iterator}")
 
 async def quote(message):
     await message.channel.send(random.choice(quotes))
@@ -203,26 +182,52 @@ Permission to add this quote was denied.""")
     
     return
 
-async def scan(message):
-    count = 0
-    print(f"id: {message.id}")
-    iterator = message
-    oldIterator = "temp"
-    while(True):
-        async for msg in message.channel.history(limit=9999999999,before=iterator):
-            count += 1
-            iterator = msg
-            if '"-' in msg.content or '" -' in msg.content:
-                #  or "-" in msg.content:
-                print(msg.content)
-                print(f"count: {count}\n")
-        if oldIterator == iterator:
-            print("done scanning")
-            break
-        oldIterator = iterator
-        print(f"last message: {msg}")
-        print(msg.content)
-        print("fetching more results")
-        print(f"iterator: {iterator}")
+async def tylerMessage(message):
+    print("it's a message from tyler")
+    if tyler[message.channel.name].msgCount == 0:
+        print("it's the first one")
+        tyler[message.channel.name].channel = message.channel
+        tyler[message.channel.name].startTime = message.created_at
+
+    tyler[message.channel.name].messages.append(message.content)
+
+    tyler[message.channel.name].msgCount += 1
+    tyler[message.channel.name].msgsSinceLastResponse += 1
+
+    print(f"tyler has now sent {tyler[message.channel.name].msgCount} messages in a row")
+
+    def check(msg):
+        print(f"went in the check, variables: \nmsgCount: {tyler[message.channel.name].msgCount}\nmsgLimit: {msgLimit}\nauthor id: {msg.author.id}\ntyler id: {tylerId}\n")
+        return msg.author.id != tylerId or (tyler[message.channel.name].msgCount >= msgLimit + 1)
+
+    if tyler[message.channel.name].msgCount >= msgLimit and tyler[message.channel.name].msgsSinceLastResponse >= responseRateCap:
+        print("msgCount is higher than the limit")
+        tyler[message.channel.name].diff = int(message.created_at.timestamp()) - int(tyler[message.channel.name].startTime.timestamp())
+        if tyler[message.channel.name].diff >= timeoutLength and tyler[message.channel.name].waiting == False:
+            print("the time difference was higher than the time limit and we are not waiting on another message to be sent")
+            if tyler[message.channel.name].msgsSinceLastResponse >= responseRateCap:
+                tyler[message.channel.name].msgsSinceLastResponse = 0
+                await tyler[message.channel.name].channel.send(response(tyler[message.channel.name].messages))
+        else:
+            print("the messages were sent within the time limit")
+            if tyler[message.channel.name].waiting == False:
+                print("we are not waiting on another message")
+                try:
+                    print("now waiting on the check...")
+                    tyler[message.channel.name].waiting = True
+                    msg = await client.wait_for('message', timeout=timeoutLength - tyler[message.channel.name].diff, check=check)
+                    if (msg.author.id != tylerId):
+                        print("someone else sent a message, abort mission")
+                        tyler[message.channel.name].reset()
+                    else:
+                        print("sending message")
+                        tyler[message.channel.name].msgsSinceLastResponse = 0
+                        await tyler[message.channel.name].channel.send(response(tyler[message.channel.name].messages))
+                except asyncio.TimeoutError:
+                    print("timed out, sending message")
+                    tyler[message.channel.name].msgsSinceLastResponse = 0
+                    await tyler[message.channel.name].channel.send(response(tyler[message.channel.name].messages))
+                print("resetting the waiting variable")
+                tyler[message.channel.name].waiting = False
 
 client.run(token)
