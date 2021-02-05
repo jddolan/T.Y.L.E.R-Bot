eb8a79336f5a45f6a4011f8299cbd2de1beabda3
jddolan <jdolan@disruptops.com>
2021-02-05 10:53:19 -0600

    seperated constants. typo fix

--- bot_commands.py
+++ bot_commands.py
@@ -3,6 +3,7 @@ import re
 import responses
 import os
 import asyncio
+from numbers import units, numbers, numberLink, emojiLink, numberEmojis
 from lenny import lenny as lennyface
 from time import sleep
 from bad_words import badWords
@@ -18,71 +19,6 @@ badUsers: list = [
     '259193538384887809',
     '138358974071308289'
 ]
-
-units: dict = {
-    'second': 1,
-    'seconds': 1,
-    'minute': 60,
-    'minutes': 60,
-    'hour': 3600,
-    'hours': 3600,
-    'day': 86400,
-    'days': 86400,
-    'week':604800,
-    'weeks': 604800,
-    'month': 2628000,
-    'months': 2628000,
-    'year': 31536000,
-    'years': 31536000
-}
-
-numbers: dict = {
-    1: ":one:",
-    2: ":two:",
-    3: ":three:",
-    4: ":four:",
-    5: ":five:",
-    6: ":six:",
-    7: ":seven:",
-    8: ":eight:",
-    9: ":nine:"
-}
-
-numberLink: dict = {
-    ":one:": 1,
-    ":two:": 2,
-    ":three:": 3,
-    ":four:": 4,
-    ":five:": 5,
-    ":six:": 6,
-    ":seven:": 7,
-    ":eight:": 8,
-    ":nine:": 9
-}
-
-emojiLink: dict = {
-    1: "1️⃣",
-    2: "2️⃣",
-    3: "3️⃣",
-    4: "4️⃣",
-    5: "5️⃣",
-    6: "6️⃣",
-    7: "7️⃣",
-    8: "8️⃣",
-    9: "9️⃣"
-}
-
-numberEmojis: dict = {
-    "1️⃣": ":one:",
-    "2️⃣": ":two:",
-    "3️⃣": ":three:",
-    "4️⃣": ":four:",
-    "5️⃣": ":five:",
-    "6️⃣": ":six:",
-    "7️⃣": ":seven:",
-    "8️⃣": ":eight:",
-    "9️⃣": ":nine:"
-}
 
 async def command(message, client):
     messageStr = message.content.split(' ')
@@ -463,7 +399,7 @@ async def timer(message, client, time, unit, prompt, options = [], ):
     
     if options != []:
         for i in range(1,len(options) + 1):
-            await newMessage.add_reaction(dmojiLink[i])
+            await newMessage.add_reaction(emojiLink[i])
 
     timeout: int = time * int(units[unit])
     print(f"timeout: {timeout}")
---
+++ numbers.py
@@ -0,0 +1,64 @@
+units: dict = {
+    'second': 1,
+    'seconds': 1,
+    'minute': 60,
+    'minutes': 60,
+    'hour': 3600,
+    'hours': 3600,
+    'day': 86400,
+    'days': 86400,
+    'week':604800,
+    'weeks': 604800,
+    'month': 2628000,
+    'months': 2628000,
+    'year': 31536000,
+    'years': 31536000
+}
+
+numbers: dict = {
+    1: ":one:",
+    2: ":two:",
+    3: ":three:",
+    4: ":four:",
+    5: ":five:",
+    6: ":six:",
+    7: ":seven:",
+    8: ":eight:",
+    9: ":nine:"
+}
+
+numberLink: dict = {
+    ":one:": 1,
+    ":two:": 2,
+    ":three:": 3,
+    ":four:": 4,
+    ":five:": 5,
+    ":six:": 6,
+    ":seven:": 7,
+    ":eight:": 8,
+    ":nine:": 9
+}
+
+emojiLink: dict = {
+    1: "1️⃣",
+    2: "2️⃣",
+    3: "3️⃣",
+    4: "4️⃣",
+    5: "5️⃣",
+    6: "6️⃣",
+    7: "7️⃣",
+    8: "8️⃣",
+    9: "9️⃣"
+}
+
+numberEmojis: dict = {
+    "1️⃣": ":one:",
+    "2️⃣": ":two:",
+    "3️⃣": ":three:",
+    "4️⃣": ":four:",
+    "5️⃣": ":five:",
+    "6️⃣": ":six:",
+    "7️⃣": ":seven:",
+    "8️⃣": ":eight:",
+    "9️⃣": ":nine:"
+}
