61459ed8bb661441456d3ea33f399b01e422212f
jddolan <jdolan@disruptops.com>
2021-02-06 23:48:45 -0600

    Update bot_commands.py

--- bot_commands.py
+++ bot_commands.py
@@ -54,7 +54,8 @@ async def command(message, client):
             word = re.sub(r'\W+', '', word)
             if word in badWords:
                 print(f"filtering a message from {message.author.name}")
-                await message.channel.send("Your message was caught by the language filter, please refrain from using shitty language. Ping me if you don't know which word got caught in the filter and I'll look into it")
+                await message.channel.send("""Your message triggered the language filter and was not processed as a result.
+If you don't know which word got caught in the filter, ping Joe (Wiz Biz) and he can look into it.""")
                 return 
     commandStr = messageStr[0]
     command = commands.get(commandStr, invalidCommand)
